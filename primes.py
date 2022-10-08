7077314a6b60d5ef88108e4fa4d6b98bb4d26c81
runxin.shao <runxin.shao@kcl.ac.uk>
2022-10-08 05:00:21 +0100

    prime exercise

--- primes.py
+++ primes.py
@@ -1,6 +1,28 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
+
+def checkPrime(number):
+    isPrime = True
+    for y in range(2, int(math.sqrt(number) + 1)):
+        if number % y == 0:
+            isPrime = False
+            break
+    return isPrime
+
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("number of primes needs to be greater than 0")
     list = []
+    list.append(2)
+    number = 3
+    while len(list) < number_of_primes:
+        if checkPrime(number):
+            list.append(number)
+        number += 1
     return list
+primes(3)
+
+
+
